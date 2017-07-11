b76818618e5716848da1fe87832f1abc09b4083b
yghjava <=>
2017-07-11 10:53:43 +0800

    commit

---
+++ common/createNodeDir.sh
@@ -0,0 +1,9 @@
+#a script just to create a directory whth four sub directories
+#author ygh
+#data 2017-07-11
+mkdir $1 &&
+cd $1 &&
+mkdir doc &&
+mkdir pictures &&
+mkdir example &&
+mkdir resource
